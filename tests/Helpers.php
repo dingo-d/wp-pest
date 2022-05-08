7f609f10623d8aa2404f33603cfc997f7e55f203
Denis Žoljom <dingo-d@users.noreply.github.com>
2022-05-08 10:59:51 +0200

    Add a helper to clean the output directory

--- tests/Helpers.php
+++ tests/Helpers.php
@@ -10,17 +10,55 @@
 
 namespace MadeByDenis\WpPestIntegrationTestSetup\Tests;
 
+use FilesystemIterator;
 use Mockery;
 use Mockery\MockInterface;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
 
 /**
  * Mockery shorthand
  *
  * @param string $class Class name to mock.
  *
+ * @since 1.0.0
+ *
  * @return \Mockery\MockInterface
  */
 function mock(string $class): MockInterface
 {
 	return Mockery::mock($class);
+}
+
+/**
+ * Used for cleaning out the cliOutput created after every CLI test
+ *
+ * @param string $dir Directory to remove.
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+function deleteOutputDir(string $dir = '') : void
+{
+	if (!$dir) {
+		$dir = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'output';
+	}
+
+	if (!\is_dir($dir)) {
+		return;
+	}
+
+	$iterator = new RecursiveDirectoryIterator($dir, FilesystemIterator::SKIP_DOTS);
+	$files = new RecursiveIteratorIterator($iterator, RecursiveIteratorIterator::CHILD_FIRST);
+
+	foreach ($files as $file) {
+		if ($file->isDir()) {
+			rmdir($file->getRealPath());
+		} else {
+			unlink($file->getRealPath());
+		}
+	}
+
+	rmdir($dir);
 }
